51032fa445bd59bab6c704451fcf5fa9557696d7
tiendd <tiendd@mobiletech.vn>
2020-06-25 14:11:49 +0700

    .

---
+++ folioreader/src/main/java/com/folioreader/ui/activity/ContentHighlightActivity.java
@@ -0,0 +1,186 @@
+package com.folioreader.ui.activity;
+
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentTransaction;
+import com.folioreader.Config;
+import com.folioreader.Constants;
+import com.folioreader.FolioReader;
+import com.folioreader.R;
+import com.folioreader.ui.fragment.HighlightFragment;
+import com.folioreader.ui.fragment.TableOfContentFragment;
+import com.folioreader.util.AppUtil;
+import com.folioreader.util.UiUtil;
+import org.readium.r2.shared.Publication;
+import android.util.Log;
+import com.folioreader.ui.fragment.UserInfoDialog;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import com.folioreader.ui.fragment.FolioPageFragment;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class ContentHighlightActivity extends AppCompatActivity {
+    private boolean mIsNightMode;
+    private Config mConfig;
+    private Publication publication;
+    private RelativeLayout btnPurchase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_content_highlight);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        publication = (Publication) getIntent().getSerializableExtra(Constants.PUBLICATION);
+
+        mConfig = AppUtil.getSavedConfig(this);
+        mIsNightMode = mConfig != null && mConfig.isNightMode();
+        initViews();
+
+
+    }
+
+
+
+    private void initViews() {
+
+        UiUtil.setColorIntToDrawable(0x7f050090, ((ImageView) findViewById(R.id.btn_close)).getDrawable());
+        findViewById(R.id.layout_content_highlights).setBackgroundDrawable(UiUtil.getShapeDrawable(mConfig.getThemeColor()));
+
+        if (mIsNightMode) {
+            findViewById(R.id.btn_contents).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.black)));
+            findViewById(R.id.btn_highlights).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.black)));
+            ((TextView) findViewById(R.id.btn_contents)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.black), mConfig.getThemeColor()));
+            ((TextView) findViewById(R.id.btn_highlights)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.black), mConfig.getThemeColor()));
+
+        } else {
+            ((TextView) findViewById(R.id.btn_contents)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getThemeColor()));
+            ((TextView) findViewById(R.id.btn_highlights)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getThemeColor()));
+            findViewById(R.id.btn_contents).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.white)));
+            findViewById(R.id.btn_highlights).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.white)));
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int color;
+            if (mIsNightMode) {
+                color = ContextCompat.getColor(this, R.color.black);
+            } else {
+                int[] attrs = {android.R.attr.navigationBarColor};
+                TypedArray typedArray = getTheme().obtainStyledAttributes(attrs);
+                color = typedArray.getColor(0, ContextCompat.getColor(this, R.color.white));
+            }
+            getWindow().setNavigationBarColor(color);
+        }
+
+        loadContentFragment();
+        findViewById(R.id.btn_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        String url = getIntent().getStringExtra(FolioReader.EXTRA_LINK);
+
+        if (url.length() == 0) {
+            btnPurchase = (RelativeLayout) findViewById(R.id.purchase_wrap);
+            btnPurchase.setVisibility(View.GONE);
+        }
+        findViewById(R.id.btn_purchase).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRemindPopup();
+                //  FragmentManager fm = getSupportFragmentManager();
+                // UserInfoDialog userInfoDialog = UserInfoDialog.newInstance("Nguyễn Văn Linh");
+                // userInfoDialog.show(fm, null);
+
+            }
+        });
+
+
+        // findViewById(R.id.btn_purchase).setOnClickListener(new View.OnClickListener() {
+        //     @Override
+        //     public void onClick(View v) {
+        //        // showRemindPopup();
+        //     }
+        // });
+
+
+        findViewById(R.id.btn_contents).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               loadContentFragment();
+            }
+        });
+
+        findViewById(R.id.btn_highlights).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadHighlightsFragment();
+            }
+        });
+    }
+
+    private void showRemindPopup() {
+        // String bookLink = getIntent().getStringExtra(FolioReader.EXTRA_LINK);
+        new AlertDialog.Builder(this)
+            .setTitle("")
+            .setMessage("Đã hết nội dung miễn phí, vui lòng mua sách tại trang web")
+
+            // Specifying a listener allows you to take an action before dismissing the dialog.
+            // The dialog is automatically dismissed when a dialog button is clicked.
+            .setPositiveButton("Mua ngay", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) { 
+                    String url = getIntent().getStringExtra(FolioReader.EXTRA_LINK);
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url)); 
+                    startActivity(i); 
+               }
+             })
+
+            // A null listener allows the button to dismiss the dialog and take no further action.
+            .setNegativeButton("Để sau", null)
+            .show();
+    }
+
+    private void loadContentFragment() {
+        findViewById(R.id.btn_contents).setSelected(true);
+        findViewById(R.id.btn_highlights).setSelected(false);
+        String bookLink = getIntent().getStringExtra(FolioReader.EXTRA_LINK);
+        TableOfContentFragment contentFrameLayout = TableOfContentFragment.newInstance(publication,
+                getIntent().getStringExtra(Constants.CHAPTER_SELECTED),
+                getIntent().getStringExtra(Constants.BOOK_TITLE), bookLink);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.parent, contentFrameLayout);
+        ft.commit();
+    }
+
+    private void loadHighlightsFragment() {
+        findViewById(R.id.btn_contents).setSelected(false);
+        findViewById(R.id.btn_highlights).setSelected(true);
+        String bookId = getIntent().getStringExtra(FolioReader.EXTRA_BOOK_ID);
+        String bookTitle = getIntent().getStringExtra(Constants.BOOK_TITLE);
+        HighlightFragment highlightFragment = HighlightFragment.newInstance(bookId, bookTitle);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ft.replace(R.id.parent, highlightFragment);
+        ft.commit();
+    }
+
+}
---
+++ folioreader/src/main/java/com/folioreader/ui/activity/FolioActivityCallback.java
@@ -0,0 +1,48 @@
+package com.folioreader.ui.activity;
+
+import android.graphics.Rect;
+import com.folioreader.Config;
+import com.folioreader.model.DisplayUnit;
+import com.folioreader.model.locators.ReadLocator;
+
+import java.lang.ref.WeakReference;
+
+public interface FolioActivityCallback {
+
+    int getCurrentChapterIndex();
+
+    String getTooltipStep();
+
+    void setTooltipStep();
+
+    ReadLocator getEntryReadLocator();
+
+    boolean goToChapter(String href);
+
+    Config.Direction getDirection();
+
+    void onDirectionChange(Config.Direction newDirection);
+
+    void storeLastReadLocator(ReadLocator lastReadLocator);
+
+    void toggleSystemUI();
+
+    void hideSystemUI();
+
+    void setDayMode();
+
+    void setNightMode();
+
+    int getTopDistraction(final DisplayUnit unit);
+
+    int getBottomDistraction(final DisplayUnit unit);
+
+    Rect getViewportRect(final DisplayUnit unit);
+
+    WeakReference<FolioActivity> getActivity();
+
+    String getStreamerUrl();
+    String getTooltipStatus();
+
+    void setTooltipStatus();
+}
